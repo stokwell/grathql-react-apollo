13219a185b68253ddc826b7787eb506d5d6cabfb
Vitaly Lyapunov <stokwell1@gmail.com>
2017-09-30 21:50:23 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:3000/graphql'
+})
+
+const client = new ApolloClient({
+  networkInterface
+})
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
+registerServiceWorker()
